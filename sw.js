ae15d2ae729ba2ec7fe6411e8b885bc073d2bd32
maker-koech <peterarapkipsony102@gmail.com>
2026-08-02 23:28:46 +0300

    Bump cache version to force old cached devices to fully refresh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sision-primary-results-app-v1";
+const CACHE_NAME = "sision-primary-results-app-v2";
 const SHELL_FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
